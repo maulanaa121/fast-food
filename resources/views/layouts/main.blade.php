fc36290b1f85c98c705ea420b55173d8b7332d45
Fajar Maulana <fm886211@gmail.com>
2026-08-21 14:16:44 +0700

    adding a CDN icon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,15 +6,16 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.7.2/css/all.min.css">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
     <title>Fast Food</title>
 </head>
 
 <body class="bg-gray-100">
-
     @include('layouts.navbar')
 
+    <h1 class="h-[1000px] pt-28">lkjkljlkjlkj</h1>
+    
+    @include('layouts.footer')
 </body>
 </html>
