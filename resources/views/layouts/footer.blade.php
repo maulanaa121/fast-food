0b16dcc087db3e663138d6aebaed8a39f2976f61
Fajar Maulana <fm886211@gmail.com>
2026-08-21 14:22:04 +0700

    adding a footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,194 @@
+<footer class="bg-[#1B1D29] font-poppins">
+
+    <div class="mx-auto max-w-[1280px] px-6 py-16 sm:px-10 lg:px-12 lg:py-20">
+
+        <div class="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-4 lg:gap-12">
+
+            {{-- Menu Links --}}
+            <div>
+                <h3 class="text-[22px] font-semibold text-white">
+                    Menu Links
+                </h3>
+
+                <div class="mt-3 h-[2px] w-[62px] bg-[#FF6527]"></div>
+
+                <ul class="mt-5 space-y-5">
+                    <li>
+                        <a href="/"
+                            class="text-[18px] text-[#9CA3BF] transition duration-200 hover:text-[#FF6527]">
+                            Home
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#about"
+                            class="text-[18px] text-[#9CA3BF] transition duration-200 hover:text-[#FF6527]">
+                            About
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#menu"
+                            class="text-[18px] text-[#9CA3BF] transition duration-200 hover:text-[#FF6527]">
+                            Menu
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#services"
+                            class="text-[18px] text-[#9CA3BF] transition duration-200 hover:text-[#FF6527]">
+                            Services
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#contact"
+                            class="text-[18px] text-[#9CA3BF] transition duration-200 hover:text-[#FF6527]">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+
+            {{-- Our Services --}}
+            <div>
+                <h3 class="text-[22px] font-semibold text-white">
+                    Our Services
+                </h3>
+
+                <div class="mt-3 h-[2px] w-[62px] bg-[#FF6527]"></div>
+
+                <ul class="mt-5 space-y-5">
+                    <li>
+                        <a href="#"
+                            class="text-[18px] text-[#9CA3BF] transition duration-200 hover:text-[#FF6527]">
+                            Web Design
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#"
+                            class="text-[18px] text-[#9CA3BF] transition duration-200 hover:text-[#FF6527]">
+                            Web Development
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#"
+                            class="text-[18px] text-[#9CA3BF] transition duration-200 hover:text-[#FF6527]">
+                            Marketing
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#"
+                            class="text-[18px] text-[#9CA3BF] transition duration-200 hover:text-[#FF6527]">
+                            Product Management
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#"
+                            class="text-[18px] text-[#9CA3BF] transition duration-200 hover:text-[#FF6527]">
+                            Graphic Design
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+
+            {{-- Information --}}
+            <div>
+                <h3 class="text-[22px] font-semibold text-white">
+                    Information
+                </h3>
+
+                <div class="mt-3 h-[2px] w-[62px] bg-[#FF6527]"></div>
+
+                <ul class="mt-5 space-y-5">
+                    <li>
+                        <a href="#"
+                            class="text-[18px] text-[#9CA3BF] transition duration-200 hover:text-[#FF6527]">
+                            About Us
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#"
+                            class="text-[18px] text-[#9CA3BF] transition duration-200 hover:text-[#FF6527]">
+                            Delivery Information
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#"
+                            class="text-[18px] text-[#9CA3BF] transition duration-200 hover:text-[#FF6527]">
+                            Privacy Policy
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#"
+                            class="text-[18px] text-[#9CA3BF] transition duration-200 hover:text-[#FF6527]">
+                            Terms And Conditions
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+
+            {{-- Contact Us --}}
+            <div>
+                <h3 class="text-[22px] font-semibold text-white">
+                    Contact us
+                </h3>
+
+                <div class="mt-3 h-[2px] w-[62px] bg-[#FF6527]"></div>
+
+                <div class="mt-5 flex flex-wrap gap-5">
+
+                    {{-- Facebook --}}
+                    <a
+                        href="#"
+                        aria-label="Facebook"
+                        class="flex h-[50px] w-[50px] items-center justify-center rounded-full bg-[#FF6527] text-white transition duration-300 hover:-translate-y-1 hover:bg-white hover:text-[#FF6527]"
+                    >
+                        <i class="fa-brands fa-facebook-f text-[21px]"></i>
+                    </a>
+
+                    {{-- Instagram --}}
+                    <a
+                        href="#"
+                        aria-label="Instagram"
+                        class="flex h-[50px] w-[50px] items-center justify-center rounded-full bg-[#FF6527] text-white transition duration-300 hover:-translate-y-1 hover:bg-white hover:text-[#FF6527]"
+                    >
+                        <i class="fa-brands fa-instagram text-[22px]"></i>
+                    </a>
+
+                    {{-- Twitter --}}
+                    <a
+                        href="#"
+                        aria-label="Twitter"
+                        class="flex h-[50px] w-[50px] items-center justify-center rounded-full bg-[#FF6527] text-white transition duration-300 hover:-translate-y-1 hover:bg-white hover:text-[#FF6527]"
+                    >
+                        <i class="fa-brands fa-twitter text-[21px]"></i>
+                    </a>
+
+                    {{-- YouTube --}}
+                    <a
+                        href="#"
+                        aria-label="YouTube"
+                        class="flex h-[50px] w-[50px] items-center justify-center rounded-full bg-[#FF6527] text-white transition duration-300 hover:-translate-y-1 hover:bg-white hover:text-[#FF6527]"
+                    >
+                        <i class="fa-brands fa-youtube text-[21px]"></i>
+                    </a>
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</footer>
