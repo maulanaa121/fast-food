672f944c2874e49d40d5e36846cf959cd5f5b8bd
Fajar Maulana <fm886211@gmail.com>
2026-08-21 16:56:28 +0700

    fix foodMenu section in mobile view

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -194,7 +194,7 @@
 
         {{-- Food Cards --}}
         <div
-            class="mt-14 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3"
+            class="mt-14 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 px-4.5 sm:px-0 "
         >
 
             {{-- Card 1 --}}
